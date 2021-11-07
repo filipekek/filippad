0cf435c146efcee09fc2f84b7ae704d749fc0e74
Filip Benes <filipb.may@gmail.com>
2021-11-07 18:12:51 +0100

    json

--- testjson.py
+++ testjson.py
@@ -5,26 +5,23 @@ with open("testdata.json") as json_file:
     z = json.load(json_file)
 
 
-physical_categories = list( z[0]['physical'].keys() )
-financial_categories = list( z[0]['financial'].keys() )
-
 a = {'name': 'average', 'physical': {'age':[],'height':[],'weight'}, 'financial':{'wealth':[],'income':[]}}
 
-all_ages = []
-all_heights = []
-all_weights = []
-all_wealth = []
-all_incomes = []
+#all_ages = []
+#all_heights = []
+#all_weights = []
+#all_wealth = []
+#all_incomes = []
 
 for u in z:
-    all_ages.append(u['physical']['age'])
+    a['physical']['age'].append(u['physical']['age'])
     all_heights.append(u['physical']['height'])
     all_weights.append(u['physical']['weight'])
     all_wealth.append(u['financial']['wealth'])
     all_incomes.append(u['financial']['income'])
 
 
-a = {'name' : 'average', 'physical': {'age': statistics.mean(all_ages), 'height': statistics.mean(all_heights), 'weight': statistics.mean(all_weights)}, 'financial': {'wealth': statistics.mean(all_wealth), 'income': statistics.mean(all_incomes)}}
+#a = {'name' : 'average', 'physical': {'age': statistics.mean(all_ages), 'height': statistics.mean(all_heights), 'weight': statistics.mean(all_weights)}, 'financial': {'wealth': statistics.mean(all_wealth), 'income': statistics.mean(all_incomes)}}
 
 with open("testaverage.json", "w") as outfile:
     json.dump(a, outfile, indent=4)
